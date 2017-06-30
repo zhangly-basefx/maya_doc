7d664e631c9e8b3584d4bdd4b8f692b9194726c8
zhangly <zhangly.basefx@gmail.com>
2017-06-30 08:41:50 +0000

    170630

--- about.py
+++ about.py
@@ -30,15 +30,30 @@ cmds.about(languageResources=True)
 
 # 返回操作系统是否为xxx
 cmds.about(linux=True)
-> linux
-
+>>>linux
 cmds.about(mac=True)
-> macOS
-
+>>>macOS
 cmds.about(ntOS=True)
-> windows
+>>>  windows
 
 # 返回Autodesk格式产品信息
 cmds.about(liveUpdate)
 >>> ("MAYA","2016","Linux64","201603180400-990260")
+
+# 返回操作系统类型，如"nt","win64","mac","linux","linux64"
+cmds.about(operatingSystem=True)
+
+# 返回系统的版本
+cmds.about(operatingSystemVersion=True)
+
+# 返回用户预设目录的位置
+cmds.about(preferences=True)
+
+# 返回maya界面语言
+cmds.about(uiLanguage=True)
+
+
+
+
+
 
