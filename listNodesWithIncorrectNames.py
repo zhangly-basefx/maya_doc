504e492f5a079a9311d9a5a17f1f5fd9e35b068e
zhangly <zhangly.basefx@gmail.com>
2017-07-03 09:10:29 +0000

    170703

---
+++ listNodesWithIncorrectNames.py
@@ -0,0 +1,5 @@
+'''
+列出场景中所有节点不正确的名称
+'''
+
+cmds.listNodesWithIncorrectNames()
---
+++ ls.py
@@ -0,0 +1,32 @@
+'''
+列出命令
+'''
+
+# 列出场景所有物体
+cmds.ls()
+
+# 列出选择的所有对象
+cmds.ls(selection=True)
+
+# 列出被选择多个对象中，最后一个被选择的元素
+cmds.ls(selection=True, tail=True)
+
+# 列出所有名为'sphere1'的对象，不会列出实例化的对象
+cmds.ls('sphere1')
+
+# 列出所有名为'sphere1'的对象，包含实例化对象
+cmds.ls('sphere1', allPaths=True)
+
+# 列出所有被选择的对象中，前缀为'group'的对象
+cmds.ls('group*', selection=True)
+
+# 列出所有的物体，灯光，摄像机
+cmds.ls(geometry=True, lights=True, cameras=True)
+
+# 列出所有的形状节点
+cmds.ls(shapes=True)
+
+# 列出所有几何形状节点，并显示它的类型
+cmds.ls(type='geometryShape', showType=True)
+
+
